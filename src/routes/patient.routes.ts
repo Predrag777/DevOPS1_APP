b126e62438f530ae8a64393af46a0db64c0459b1
Predrag777 <predragbrajovicSS@gmail.com>
2025-01-25 13:02:45 +0100

    config file needs changes

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,5 +1,19 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { createPatient, deletePatient, getPatient, getPatients, updatePatient } from '../controller/patient.controller';
 
 const patientRoutes = Router();
+
+patientRoutes.route('/ss')
+    .get(getPatient);
+
+/*
+patientRoutes.route('/')
+  .get(getPatients) 
+  .post(createPatient);
+
+patientRoutes.route('/:patientId')
+  .get(getPatient)
+  .put(updatePatient)
+  .delete(deletePatient);*/
 
 export default patientRoutes;
