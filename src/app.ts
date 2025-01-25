2fcb34f0ce37207d52a84eff1ed39284d0b0c900
Predrag777 <predragbrajovicSS@gmail.com>
2025-01-25 07:16:57 +0100

    esponses

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Application } from "express";
 import cors from "cors";
 import ip from "ip";
 
+import { HttpResponse } from "./domain/response";
+import {Code} from "./enum/code.enum";
+import {Status} from "./enum/status.enum";
 export class App {
     private readonly app: Application;
     private readonly APPLICATION_RUNNING = 'application is running on: ';
@@ -17,7 +20,7 @@ export class App {
         this.app.listen(this.port);
         console.info(`${this.APPLICATION_RUNNING} ${ip.address()}:${this.port}`)
     }
-    
+
     private middleWare(): void{
         this.app.use(cors({origin: '*'}));// NEver do this in real life!!!
         this.app.use(express.json());
@@ -26,7 +29,7 @@ export class App {
 
     routes(){
         this.app.use('/patients', (req, res)=>{});
-        this.app.use('/', (req, res)=>{res.status(200).send({message: 'Server is up'})});
+        this.app.use('/', (req, res)=>{res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, "Welcome to the Patients API V1.0"))});
         this.app.use('*', (req, res)=>{res.status(200).send({message: this.ROUTE_NOT_FOUND})});
     }
 }
